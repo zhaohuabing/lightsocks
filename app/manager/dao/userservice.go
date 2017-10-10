0f3c1d4bea652e6d720c97f480626c209c6a9364
wuhaolin <wuhaolin@meituan.com>
2017-10-10 19:37:23 +0800

    remove app code

--- app/manager/dao/userservice.go
+++
@@ -1,8 +0,0 @@
-package dao
-
-import "github.com/gwuhaolin/lightsocks/app/manager/model"
-
-func CreateUserService(userService *model.UserService) error {
-	_, err := connPool.Exec("UserService.Create", userService.Email, userService.Addr, userService.UUID, userService.Device)
-	return err
-}
--- app/server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/gwuhaolin/lightsocks/app/server/rpc/listener"
-)
-
-func main() {
-	listener.ListenRPC()
-}
